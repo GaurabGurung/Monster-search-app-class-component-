8dfe3beea14ca081b27ed1b74255ea635cc6e362
Gaurab Gurung <gaurabgurung181@gmail.com>
2023-05-13 19:54:39 +1000

    monster-app-class-component

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+import logo from './logo.svg';
+import SearchBox from './components/search-box/search-box.component';
+import CardList from './components/card-list/card-list.component';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+
+  onSearchChange = (event)=>{
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(()=>{
+      return {
+        searchField
+      }
+    })
+  }
+  
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=> response.json())
+    .then((users)=> this.setState(
+      ()=>{
+        return {monsters: users};
+      },
+
+      ))
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
+    return (
+      <div className="App">
+        <h1 className='app-title'> Monster Rolodex</h1>
+        <SearchBox 
+          onChangeHandler = { onSearchChange } 
+          placeHolder= " search monsters " 
+          className = ' monsters-search-box '
+          />
+        <CardList monsters ={ filteredMonsters }/>
+      </div>
+    )
+  }
+}
+
+export default App;
